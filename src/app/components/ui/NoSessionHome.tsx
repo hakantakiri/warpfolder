4e02765ff8304838b1e6fb1755792bdb60669a58
hakan <hakan@y540.leg>
2023-11-17 18:25:50 -0500

    WIP: NO session flow

--- src/app/components/ui/NoSessionHome.tsx
+++ src/app/components/ui/NoSessionHome.tsx
@@ -26,12 +26,10 @@ export const NoSessionHome = () => {
 
 			<div
 				ref={modalRef}
-				className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center hidden"
+				className="flex items-center justify-center fixed inset-0 bg-gray-800 bg-opacity-75 hidden"
 			>
 				<div className="bg-white p-8 rounded shadow-lg w-1/2">
-					<p className="text-xl font-semibold mb-4">
-						Modal Content Goes Here
-					</p>
+					<p className="text-xl font-semibold mb-4">Modal Content Goes Here</p>
 					<button
 						className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
 						onClick={closeModal}
