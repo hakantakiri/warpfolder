ab8327d00a01e19f524724943260a3649e96860c
Hakan <hakan@mbp18.com>
2023-11-17 19:26:05 -0500

    Add first time home state

--- src/app/components/ui/NoSessionHome.tsx
+++ src/app/components/ui/NoSessionHome.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { MouseEvent, useRef } from "react"
 import { PrimaryButton } from "./uploadButton"
+import LogoSvg from "../icons/logo.svg"
+import Image from "next/image"
 
 export const NoSessionHome = () => {
 	const modalRef = useRef<HTMLDivElement>(null)
@@ -16,12 +18,26 @@ export const NoSessionHome = () => {
 	return (
 		<section className="flex h-screen items-center justify-center">
 			<div className="flex flex-col items-center space-y-10">
+				<div className="flex space-x-4">
+					<Image src={LogoSvg} alt="logo" />
+					<span className="flex justify-start items-center text-blue-400 ">
+						<h1>
+							WARP <br />
+							FOLDER
+						</h1>
+					</span>
+				</div>
 				<PrimaryButton text="UPLOAD YOUR FIRST FILE" />
-				<hr />
-				<span>or</span>
-				<a href="#" onClick={openModal}>
-					open an existing folder
-				</a>
+				<span className="space-x-2">
+					<span>or</span>
+					<a
+						className=" text-purple-400 hover:underline active:underline"
+						href="#"
+						onClick={openModal}
+					>
+						open an existing folder
+					</a>
+				</span>
 			</div>
 
 			<div
@@ -29,7 +45,9 @@ export const NoSessionHome = () => {
 				className="flex items-center justify-center fixed inset-0 bg-gray-800 bg-opacity-75 hidden"
 			>
 				<div className="bg-white p-8 rounded shadow-lg w-1/2">
-					<p className="text-xl font-semibold mb-4">Modal Content Goes Here</p>
+					<p className="text-xl font-semibold mb-4">
+						Modal Content Goes Here
+					</p>
 					<button
 						className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
 						onClick={closeModal}
--- src/app/components/ui/uploadButton.tsx
+++ src/app/components/ui/uploadButton.tsx
@@ -9,10 +9,10 @@ export const PrimaryButton = (props: PrimaryButtonProps) => {
 	return (
 		<button
 			className="flex justify-center items-center h-30 w-60 p-4 
-            shadow-lg border-2 rounded-xl text-white shadow-blue-500
+            shadow-xl border-2 rounded-3xl text-white
             border-blue-400 bg-blue-400
             hover:bg-blue-700 hover:border-blue-700
-            active:shadow-xl
+            active:shadow-md
 
 
 "
